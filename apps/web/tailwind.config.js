818aa80e13c866478dcb9c9bd85bf06c38f24420
aryundhazza <aryundhazza@gmail.com>
2024-10-17 16:35:53 +0700

    chore: first init

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx,mdex}',
+    './src/components/**/*.{js,jsx,ts,tsx,mdex}',
+    './src/pages/**/*.{js,jsx,ts,tsx,mdex}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
