5e639e91397fa465d53f3e50fc1ca2e5ff324f54
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-20 17:53:41 +0700

    feat: migrate first init

---
+++ apps/api/src/routers/user.router.ts
@@ -0,0 +1,21 @@
+import { UserController } from '@/controllers/user.controller';
+import { Router } from 'express';
+
+export class UserRouter {
+  private router: Router;
+  private userController: UserController;
+
+  constructor() {
+    this.userController = new UserController();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.get('/', this.userController.getUsers);
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
