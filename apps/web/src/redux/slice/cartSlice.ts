511fffb3818f4b5fe200ca960c3b399b0e9bb460
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-20 12:33:11 +0700

    feat(redux): add redux persist

---
+++ apps/web/src/redux/slice/cartSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+interface ICartItem {
+    id: number
+    productId: number
+    quantity: number
+    totalPrice: number
+}
+
+const initialState: ICartItem[] = []
+
+export const cartSlice = createSlice({
+    name: "ItemCart",
+    initialState,
+    reducers:{
+        addItemCart: (state, action: PayloadAction<ICartItem>) => {
+            const { productId, quantity, totalPrice } = action.payload
+            const isItemExist = state.findIndex((item) => item.productId === productId)
+            if(isItemExist !== -1) {
+                state[isItemExist].quantity += quantity
+                return state
+            } else {
+                return [...state, {id: 1, productId, quantity, totalPrice}]
+            }
+        },
+        
+        resetCart: () => {
+            return []
+        }, 
+    },
+
+})
+
+export const {addItemCart, resetCart} = cartSlice.actions
+export default cartSlice.reducer
