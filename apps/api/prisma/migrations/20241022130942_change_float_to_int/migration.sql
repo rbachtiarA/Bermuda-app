40d4f372eb5ffa45e7c4354e3a1a07f37db5766a
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-22 20:13:39 +0700

    fix(prisma): replace Float to Int

---
+++ apps/api/prisma/migrations/20241022130942_change_float_to_int/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `storeId` on the `product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `product` DROP COLUMN `storeId`,
+    MODIFY `price` INTEGER NOT NULL;
