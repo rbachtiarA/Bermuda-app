ed105e110149b3dfbc5c5a3d16bb0d251315cc8a
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-23 13:58:02 +0700

    fix(product): delete static productList

--- apps/web/src/app/product/page.tsx
+++ apps/web/src/app/product/page.tsx
@@ -3,14 +3,6 @@ import ProductList from "@/components/product/productList"
 import { IProduct } from "@/type/product"
 
 export default function page() {
-    
-    const productList: IProduct[] = [
-        {id: 1, name: 'Apple', price: 5000},
-        {id: 2, name: 'Banana', price: 7000},
-        {id: 3, name: 'Carrot', price: 4000},
-        {id: 4, name: 'Durian', price: 20000},
-        {id: 5, name: 'Eggplant', price: 10000},
-    ]
 
     return (
         <section>
