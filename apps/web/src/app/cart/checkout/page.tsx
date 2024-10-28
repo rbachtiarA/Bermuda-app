6ef35f8a15e6f8c3a93eca1bec61476318be0e22
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-28 11:16:23 +0700

    feat(checkout): add checkout page

---
+++ apps/web/src/app/cart/checkout/page.tsx
@@ -0,0 +1,11 @@
+import AddressessList from "@/components/checkout/addressList";
+import CheckoutList from "@/components/checkout/checkoutList";
+import CheckoutWrapper from "@/components/checkout/checkoutWrap";
+
+export default function page() {
+    return (
+        <section className="flex justify-center items-center my-4 ">
+            <CheckoutWrapper />
+        </section>
+    )
+}
