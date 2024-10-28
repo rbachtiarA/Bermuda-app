4d470eda9373f885e6cf03f88b3e36257a66e668
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-10-28 11:15:49 +0700

    fix(bottomNavbar): fix some classname

--- apps/web/src/components/bottomNavbar/bottomNavbar.tsx
+++ apps/web/src/components/bottomNavbar/bottomNavbar.tsx
@@ -13,7 +13,7 @@ const roboto = Roboto({
 export default function BottomNavbar () {
     const cart = useAppSelector(state => state.cart)
     return (
-        <div className="w-full sticky bottom-0" style={roboto.style}>
+        <div className="w-full sticky bottom-0 z-[100]" style={roboto.style}>
             <ul className="grid grid-cols-4 divide-x divide-white text-center py-1 bg-slate-200 justify-center items-center">
                 <li className="w-full">
                     <LinkButtonBottomNavbar label='Profile' href="/user" imgsrc="/icon-user-profile.svg" imgalt="user" />
