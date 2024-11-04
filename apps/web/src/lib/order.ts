2ee25e31b87bd509e1201b1ec246a1bb70f1df58
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-11-04 07:05:56 +0700

    feat(lib): function to call order router

--- apps/web/src/lib/order.ts
+++ apps/web/src/lib/order.ts
@@ -3,4 +3,50 @@ export const getUserAddressess = async (userId: number) => {
     const { status, data } = await res.json()
 
     return data
-} 
+}
+
+export const postOrder = async (
+    userId: number, 
+    totalAmount: number, 
+    shippingCost: number, 
+    addressId: number,
+    storeId: number,
+    methodPayment: 'Transfer' | 'Gateway') => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}order/neworder`, {
+            method:'POST',
+            body: JSON.stringify({userId, totalAmount, shippingCost, addressId, methodPayment, storeId}),
+            headers: {
+                "Content-type":"application/json"
+            }
+        })
+        
+        const { status, order, msg } = await res.json()
+        return {status, order, msg }
+    }
+
+export const getMidtransToken = async (userId: number, orderId: number) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}order/user/${userId}/order/${orderId}`)
+    const { status, msg } = await res.json()
+    if(status === 'ok') {
+        return msg.Payment.token
+    }
+    return msg
+}
+
+export const getMidtransStatus = async (orderId: number) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}order/gateway/status/${orderId}`, {cache: "no-cache"})
+    const { status, midtrans }:{status: 'ok' | 'NOT_FOUND' ,midtrans: 'pending' | 'expire' | 'settlement' | null} = await res.json()
+    
+    // status = NOT_FOUND || ok, midtrans = 'pending' | 'expire' | 'settlement' | null
+    return {status , midtrans}
+}
+export const getOrderPendingPayment = async (userId: number) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}order/pending/${userId}`, { next: { revalidate: 1 } })
+    
+    const { status, msg, order } = await res.json()
+    if(msg === 'FOUND') {
+        return order
+    } else {
+        return false
+    }
+}
