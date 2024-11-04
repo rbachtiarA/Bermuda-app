3be66606057af0b905497781ae25a66b2bed291d
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-11-04 07:12:35 +0700

    feat(order): add controllers order

--- apps/api/src/controllers/order.controller.ts
+++ apps/api/src/controllers/order.controller.ts
@@ -1,50 +1,318 @@
-import { Request, Response } from 'express';
+import { json, Request, Response } from 'express';
+import  midtrans  from '../services/midtrans.js'
 import prisma from '@/prisma';
-
 
 export class OrderController {
     async createNewOrder(req: Request, res: Response) {
         try {
-            const { userId, totalAmount, addressId, methodPayment } = req.body
+            const { userId, totalAmount, shippingCost, addressId, methodPayment, storeId } = req.body
+            console.log(storeId);
             
-            //checkout is array of {productId, quantity, price}
-            const { checkout }: { checkout: {productId: number, addressId: number, price: number}[] } = req.body
-            const convertToOrderItems = checkout.map((item) => item)
-            //get user
+            //get user data
             const user = await prisma.user.findUnique({
                 where: { id: +userId }
+                
             })
-            if(!user) throw 'User not valid'
+            if(!user) throw 'User is invalid'
 
-            //create order, payment, and orderItem
-            const order = await prisma.order.create({
-                data: {
-                    userId,
-                    addressId,
-                    status: methodPayment === 'PAYMENT_GATEWAY'? 'Confirmed' : 'PendingPayment',
-                    totalAmount,
-                    shippingOptionId: 1,
-                    Payment: {
-                        create: {
-                            amountPaid: totalAmount,
-                            paymentMethod: methodPayment,
-                            isConfirmed: methodPayment === 'PAYMENT_GATEWAY'? true : false,
-                            confirmedAt: methodPayment === 'PAYMENT_GATEWAY'? new Date() : null,
-                        }
-                    },
-                    orderItems: {
-                        createMany: {
-                            data: [
-                                // {productId, quantity, price, discountValue}
-                            ]
+            //get user checkout item
+            const checkoutItem = await prisma.checkout.findUnique({
+                where: { userId: userId },
+                include: {
+                    CartItem: {
+                        include: {
+                            product: true
                         }
                     }
                 }
             })
+            if(!checkoutItem) throw 'Checkout is invalid'
 
+            //convert checkout item to array of {productId, quantity, price, }
+            const orderItem = checkoutItem?.CartItem.map((item) => {
+                return { productId: item.productId, quantity: item.quantity, price: item.product.price, discountValue: 0 }
+            })
+            if(!orderItem) throw 'Order is Invalid'
+
+            
+            const order = await prisma.$transaction(async (tx) => {
+                //expiredDate
+                const THREE_HOURS_EXPIRED = 60 * 60 * 3 * 1000
+
+                //reduce quantity stock
+                for (const item of orderItem) {
+                    const existStock = await tx.stock.findFirst({
+                        where: {
+                            AND: {
+                                productId: item.productId,
+                                storeId: +storeId
+                            }
+                        }
+                    })
+                    if(!existStock) throw 'Some product stock is invalid'
+                    if(existStock!.quantity - item.quantity < 0) throw 'Product stock is insufficient'
+
+                    await tx.stock.update({
+                        where: {
+                            id: existStock!.id
+                        },
+                        data: {
+                            quantity: existStock!.quantity - item.quantity
+                        }
+                    })   
+                }
+
+                //create order, payment, and orderItem
+                const order = await tx.order.create({
+                    data: {
+                        userId,
+                        addressId,
+                        status: 'PendingPayment',
+                        totalAmount: totalAmount,
+                        shippingOptionId: 1,
+                        storeId,
+                        Payment: {
+                            create: {
+                                amountPaid: totalAmount,
+                                paymentMethod: methodPayment,
+                                expiredDate: new Date(Date.now() + THREE_HOURS_EXPIRED),
+                                isConfirmed: false,
+                                confirmedAt: null,
+                            }
+                        },
+                        orderItems: {
+                            createMany: {
+                                // {productId, quantity, price, discountValue}
+                                data: [...orderItem]
+                            }
+                        }
+                    }
+                })
+                
+                //creating midtrans item details
+                const itemDetails = checkoutItem.CartItem.map((item) => {
+                    return { id: item.productId, name: item.product.name.substring(0,49), quantity: item.quantity, price: item.product.price }
+                })
+
+                //if methodPayment is Gateway, generate token midtrans
+                if(order && methodPayment === 'Gateway') {
+                    
+                    //midtrans body
+                    const parameter = {
+                        transaction_details: {
+                            order_id: `ORDER_${order.id}`,
+                            gross_amount: totalAmount
+                        },
+                        item_details: [...itemDetails, {
+                            name: 'shipping Cost',
+                            price: shippingCost,
+                            quantity: 1
+                        }] ,
+                        customer_details: {
+                            first_name: user.name
+                        },
+                        expiry: {
+                            unit: "hour",
+                            duration: 3
+                        }
+                    }
+
+                    const midtransTransaction = await midtrans.snap.createTransaction(parameter)
+                    
+                    if(!midtransTransaction) return res.status(200).send({status:'failed', msg: 'Please Choose Gateway Service first' })
+                    const payment = await tx.payment.update({
+                        where: {
+                            orderId: order.id
+                        },
+                        data: {
+                            token: midtransTransaction.token
+                        }
+                    })
+                }
+
+                return { order }
+            })
+            
+            return res.status(201).send({
+                status: 'order success',
+                order
+            })
+        } catch (error) {
+            return res.status(400).send({
+                status: 'error',
+                msg: error
+            })
+        }
+    }
+
+    async getOrderById(req: Request, res: Response) {
+        try {
+            const { userId, orderId } = req.params
+            const order = await prisma.order.findUnique({
+                where: {
+                    id: +orderId,
+                    userId: +userId
+                },
+                include: {
+                    Payment: true
+                }
+            })
+
+            if(!order) throw 'Cant find order'
+            return res.status(200).send({
+                status: 'ok',
+                msg: order
+            })
 
         } catch (error) {
-            
+            return res.status(401).send({
+                status: 'error',
+                msg: error
+            })
         }
+    }
+
+    // get order which have status 'pending', return msg FOUND / NOT FOUND and Order with Order & Payment
+    async getPendingOrder(req: Request, res: Response) {
+        try {
+            const { userId } = req.params
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: +userId
+                }
+            })
+            if(!user) throw 'User is not valid'
+
+            const pendingOrder = await prisma.order.findFirst({
+                where: {
+                    userId: user.id,
+                    status: 'PendingPayment'
+                }, 
+                include : {
+                    Payment: true,
+                    Address: true,
+                    orderItems: {
+                        include: {
+                            product: true
+                        }
+                    },
+                }
+            })
+
+            let responseSend;
+            if(pendingOrder) {
+                responseSend = { status: 'ok', msg: 'FOUND', order: pendingOrder }
+            } else {
+                responseSend = { status: 'ok', msg: 'NOTFOUND' }
+            }
+
+            return res.status(200).send(responseSend)
+            
+        } catch (error) {
+            return res.status(400).send({
+                status: 'error',
+                msg: error
+            })
+        }
+    }
+
+    // get midtrans transaction status, from 'orderId', 
+    //      if status "expire" or "cancel" update order status with 'cancelled', and return all order items quantity to store quantity
+    //      if status "settlement" update order status with 'confirmed' and update payment isConfirmed to true & confirmedAt to date.now()
+    async getMidtransStatus(req:Request, res: Response) {
+        try {
+            const { orderId } = req.params 
+            
+            //check order exist
+            const existOrder = await prisma.order.findUnique({
+                where: { id: +orderId }
+            })
+            if(!existOrder) throw 'Order id is invalid'
+
+            //get midtransStatus, if customer didnt pick one of midtrans service will return 404
+            const midtransStatus = await midtrans.snap.transaction.status(`ORDER_${orderId}`)            
+
+            //convert status to converStatus, which for grouping purpose
+            const status = midtransStatus?.transaction_status
+            const convertStatus = status === 'cancel'? 'expire' : status;
+
+            //this condition to prevent unecessary multiple running proccessed
+            if((convertStatus === 'expire' && existOrder.status !== 'Cancelled') || (convertStatus === 'settlement' && existOrder.status !== 'Confirmed')) {
+                switch (convertStatus) {
+                    // if expire, status transaction will be 'cancelled' and any ordered items quantity will revert back to stock store
+                    case 'expire':
+                        console.log('proccess of cancelling order');
+                        
+                        await prisma.$transaction(async (tx) => {
+                            const updatedOrder = await tx.order.update({
+                                where: { id: +orderId },
+                                include: { orderItems: true },
+                                data: {
+                                    status: 'Cancelled',
+                                }
+                            })
+
+                            // code for update store quantity with cancelled order Items
+                            const cancelledOrderItems = updatedOrder.orderItems.map((item) => {
+                                return { productId: item.productId, quantity: item.quantity }
+                            })
+                            
+                            // update all quantity stock back to store
+                            for (const item of cancelledOrderItems) {
+                                const existStock = await tx.stock.findFirst({
+                                    where: {
+                                        AND: {
+                                            productId: item.productId,
+                                            storeId: updatedOrder.storeId
+                                        }
+                                    }
+                                })
+                                if(!existStock) throw 'Something wrong when addedd product quantity to store'
+
+                                await tx.stock.update({
+                                    where: { id: existStock.id },
+                                    data: { quantity: existStock.quantity + item.quantity }
+                                })
+                            } 
+                        })
+                        
+                        break;
+                    
+                    case 'settlement':
+                        // update status of order to confirmed
+                        const updateOrder = await prisma.order.update({
+                            where: {
+                                id: +orderId
+                            },
+                            data: {
+                                status: 'Confirmed',
+                                Payment: {
+                                    update: {
+                                        isConfirmed: true,
+                                        confirmedAt: new Date(Date.now())
+                                    }
+                                }
+                            }
+                        })
+                    default:
+                        break;
+                }
+            }
+            
+            return res.status(200).send({
+                status: 'ok',
+                midtrans: status
+            })
+        } catch (error) {
+            console.log(error);
+            
+            return res.status(200).send({
+                status: 'not_found',
+                midtrans: null, 
+                msg:'You need to select your transaction service first'
+            })
+        }
+
+
     }
 }
